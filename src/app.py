c4b9ad848f9535d5b366b156a3ef396b99137bc1
alejo <alejoviviani12@gmail.com>
2025-09-14 16:56:22 -0300

    Incorporacion del dashboard

--- src/app.py
+++ src/app.py
@@ -1,38 +1,94 @@
 from flask import Flask, redirect, render_template, url_for, request
 from models.usuariosModel import Usuario, db
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db.init_app(app)
+
 
 @app.route("/crearDB")
 def crearDB():
     db.create_all()
     return redirect(url_for("login"))
 
-@app.route("/login", methods=['POST', 'GET'])
-def login():
-#    if request.method == 'GET':
-    return render_template("login.html")
-#   else:
-#        email = request.form['email']   
-#        password = request.form['password']
-        
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        correo = request.form["email"]
+        password = request.form["password"]
+        usuario = db.session.query(Usuario).filter_by(email=correo).first()
+        if not usuario:
+            return render_template(
+                "login.html", mensajeError="El mail ingreso no está registrado"
+            )
+        verificacionContraseña = usuario.verificarContraseña(password)
+        if not verificacionContraseña:
+            return render_template(
+                "login.html", mensajeError="La contraseña ingresada es incorrecta"
+            )
+        return redirect(url_for("dashboard", id=usuario.id_usuario))
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("register.html")
     else:
-        nombre = request.form['name']    
-        mail = request.form['email']    
-        contraseña = request.form['password']    
+        nombre = request.form["name"]
+        mail = request.form["email"]
+        contraseña = request.form["password"]
 
-        nuevoUsuario = Usuario(nombre, mail, contraseña, 'admin')
-        db.session.add(nuevoUsuario)
-        db.session.commit()
-        return redirect(url_for("login"))
+        verificacionMail = db.session.query(Usuario).filter_by(email=mail).first()
+        if verificacionMail:
+            return render_template(
+                "register.html", mensajeError="Ya existe una cuenta con el mismo mail"
+            )
+        else:
+            nuevoUsuario = Usuario(nombre, mail, contraseña, "admin")
+            nuevoUsuario.crearHash()
+            db.session.add(nuevoUsuario)
+            db.session.commit()
+            return redirect(url_for("login"))
 
+
+@app.route("/dashboard")
+def dashboard():
+    id = request.args.get("id", type=int)
+    usuario = db.session.query(Usuario).filter_by(id_usuario=id).first()
+    if not usuario:
+        mensajeError = "Lo sentimos, no encontramos el usuario ingresado"
+        return redirect(url_for("notFount404", mensaje=mensajeError))
+    return render_template("dashboard.html", rol=usuario.rol)
+
+
+@app.route("/notFount404")
+def notFount404():
+    mensaje = request.args.get("mensaje", type=str)
+    return render_template("404.html", mensajeError=mensaje)
+
+
+@app.route("/clients")
+def clients():
+    return render_template("clientes/verClientes.html")
+
+
+@app.route("/products")
+def products():
+    return render_template("productos/verProductos.html")
+
+
+@app.route("/invoices")
+def invoices():
+    return render_template("facturas/verFacturas.html")
 
 
 if __name__ == "__main__":
--- src/models/usuariosModel.py
+++ src/models/usuariosModel.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
+
 
 class Usuario(db.Model):
     __tablename__ = "usuario"
 
-    def __init__ (self, nombre, email, password, rol):
+    def __init__(self, nombre, email, password, rol):
         self.nombre = nombre
         self.email = email
         self.password = password
@@ -17,3 +19,13 @@ class Usuario(db.Model):
     password = db.Column(db.String(50), nullable=False)
     rol = db.Column(db.String(50), nullable=False)
 
+    def crearHash(self):
+        contraseñaConHash = generate_password_hash(self.password)
+        self.password = contraseñaConHash
+        return True
+
+    def verificarContraseña(self, password):
+        verificacion = check_password_hash(self.password, password)
+        if not verificacion:
+            return False
+        return True
