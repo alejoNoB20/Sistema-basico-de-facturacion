eb25ea260339de30478e256b9db569671de6b827
alejo <alejoviviani12@gmail.com>
2025-09-14 17:02:37 -0300

    awd

--- src/models/usuariosModel.py
+++ src/models/usuariosModel.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 
@@ -14,18 +13,7 @@ class Usuario(db.Model):
         self.rol = rol
 
     id_usuario = db.Column(db.Integer, primary_key=True, nullable=False)
-    nombre = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=True)
-    password = db.Column(db.String(50), nullable=False)
-    rol = db.Column(db.String(50), nullable=False)
-
-    def crearHash(self):
-        contraseñaConHash = generate_password_hash(self.password)
-        self.password = contraseñaConHash
-        return True
-
-    def verificarContraseña(self, password):
-        verificacion = check_password_hash(self.password, password)
-        if not verificacion:
-            return False
-        return True
+    nombre = db.Column(db.String())
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(50))
+    rol = db.Column(db.String(50))
